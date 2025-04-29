08519c7fa187bd4cf36f3abb8d6ce4e0d8627f4a
garyzhen704 <gzhen@csu.fullerton.edu>
2025-04-29 09:48:15 -0700

    Implement basic plagiarism detection with multiple string matching algorithms

---
+++ person2.py
@@ -0,0 +1,39 @@
+from rabin_karp import rabin_karp
+from KMP import kmp_search
+from naive import naive_search
+
+def break_into_phrases(text, phrase_length=5):
+    words = text.split()
+    phrases = []
+    for i in range(len(words) - phrase_length + 1):
+        phrase = " ".join(words[i:i+phrase_length])
+        phrases.append(phrase)
+    return phrases
+
+def find_matches(doc1, doc2):
+    phrases = break_into_phrases(doc1)
+    matches_rabin = []
+    matches_kmp = []
+    matches_naive = []
+
+    for phrase in phrases:
+        rabin_positions = rabin_karp(doc2, phrase)
+        kmp_positions = kmp_search(doc2, phrase)
+        naive_positions = naive_search(doc2, phrase)
+        
+        if rabin_positions:
+            matches_rabin.append(phrase)
+        if kmp_positions:
+            matches_kmp.append(phrase)
+        if naive_positions:
+            matches_naive.append(phrase)
+
+    return {
+        "Rabin-Karp": matches_rabin,
+        "KMP": matches_kmp,
+        "Naive": matches_naive
+    }
+
+def search_phrase_in_document(document, phrase):
+    return len(naive_search(document, phrase)) > 0
+
